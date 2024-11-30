1498d5ee2f022bb9569be56f30b22e79587903e9
Roger Burton <bur@smartbe.be>
2024-11-30 18:05:37 +0100

    Update db.js

--- data/db.js
+++ data/db.js
@@ -1,1 +1,19 @@
-
+const commonXSD=['BDNDR-CCTS_CCT_SchemaModule-1.1.xsd', 'BDNDR-UnqualifiedDataTypes-1.1.xsd', 'UBL-CommonAggregateComponents-2.4.xsd'
+               , 'UBL-CommonBasicComponents-2.4.xsd', 'UBL-CommonExtensionComponents-2.4.xsd', 'UBL-CommonSignatureComponents-2.4.xsd'
+               , 'UBL-ExtensionContentDataType-2.4.xsd', 'UBL-QualifiedDataTypes-2.4.xsd', 'UBL-SignatureAggregateComponents-2.4.xsd'
+               , 'UBL-SignatureBasicComponents-2.4.xsd', 'UBL-XAdES01903v132-201601-2.4.xsd', 'UBL-XAdES01903v141-201601-2.4.xsd'
+               , 'UBL-xmldsig-core-schema-2.4.xsd', 'UBL-xmldsig1-schema-2.4.xsd', 'UBL-xmldsig11-schema-2.4.xsd']
+const mainXSD=['UBL-Invoice-2.4.xsd','UBL-ApplicationResponse-2.4.xsd', 'UBL-AttachedDocument-2.4.xsd', 'UBL-BusinessCard-2.4.xsd'
+             , 'UBL-BusinessInformation-2.4.xsd', 'UBL-Catalogue-2.4.xsd', 'UBL-CatalogueDeletion-2.4.xsd'
+             , 'UBL-CatalogueItemSpecificationUpdate-2.4.xsd', 'UBL-CataloguePricingUpdate-2.4.xsd', 'UBL-CatalogueRequest-2.4.xsd'
+             , 'UBL-ContractNotice-2.4.xsd', 'UBL-CreditNote-2.4.xsd', 'UBL-DebitNote-2.4.xsd'
+             , 'UBL-DespatchAdvice-2.4.xsd', 'UBL-DocumentStatus-2.4.xsd', 'UBL-DocumentStatusRequest-2.4.xsd'
+             , 'UBL-Enquiry-2.4.xsd', 'UBL-EnquiryResponse-2.4.xsd', 'UBL-ExceptionCriteria-2.4.xsd'
+             , 'UBL-ExceptionNotification-2.4.xsd', 'UBL-FulfilmentCancellation-2.4.xsd'
+             ,  'UBL-ItemInformationRequest-2.4.xsd', 'UBL-Order-2.4.xsd', 'UBL-OrderCancellation-2.4.xsd'
+             , 'UBL-OrderChange-2.4.xsd', 'UBL-OrderResponse-2.4.xsd', 'UBL-OrderResponseSimple-2.4.xsd'
+             , 'UBL-PurchaseReceipt-2.4.xsd', 'UBL-QualificationApplicationRequest-2.4.xsd'
+             , 'UBL-QualificationApplicationResponse-2.4.xsd', 'UBL-Quotation-2.4.xsd', 'UBL-ReceiptAdvice-2.4.xsd'
+             , 'UBL-Reminder-2.4.xsd', 'UBL-RemittanceAdvice-2.4.xsd', 'UBL-RequestForQuotation-2.4.xsd'
+             , 'UBL-RetailEvent-2.4.xsd', 'UBL-SelfBilledCreditNote-2.4.xsd', 'UBL-SelfBilledInvoice-2.4.xsd'
+const partialURL = "https://cdn.jsdelivr.net/gh/rogerburton/smartApp@main/xsd/" ;
