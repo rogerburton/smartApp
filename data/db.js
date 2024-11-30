0257af7c36be660a47743df42bf8a708a5ca10d0
Roger Burton <bur@smartbe.be>
2024-11-30 18:07:08 +0100

    Update db.js

--- data/db.js
+++ data/db.js
@@ -2,7 +2,7 @@ const commonXSD=['BDNDR-CCTS_CCT_SchemaModule-1.1.xsd', 'BDNDR-UnqualifiedDataTy
                , 'UBL-CommonBasicComponents-2.4.xsd', 'UBL-CommonExtensionComponents-2.4.xsd', 'UBL-CommonSignatureComponents-2.4.xsd'
                , 'UBL-ExtensionContentDataType-2.4.xsd', 'UBL-QualifiedDataTypes-2.4.xsd', 'UBL-SignatureAggregateComponents-2.4.xsd'
                , 'UBL-SignatureBasicComponents-2.4.xsd', 'UBL-XAdES01903v132-201601-2.4.xsd', 'UBL-XAdES01903v141-201601-2.4.xsd'
-               , 'UBL-xmldsig-core-schema-2.4.xsd', 'UBL-xmldsig1-schema-2.4.xsd', 'UBL-xmldsig11-schema-2.4.xsd']
+               , 'UBL-xmldsig-core-schema-2.4.xsd', 'UBL-xmldsig1-schema-2.4.xsd', 'UBL-xmldsig11-schema-2.4.xsd'];
 const mainXSD=['UBL-Invoice-2.4.xsd','UBL-ApplicationResponse-2.4.xsd', 'UBL-AttachedDocument-2.4.xsd', 'UBL-BusinessCard-2.4.xsd'
              , 'UBL-BusinessInformation-2.4.xsd', 'UBL-Catalogue-2.4.xsd', 'UBL-CatalogueDeletion-2.4.xsd'
              , 'UBL-CatalogueItemSpecificationUpdate-2.4.xsd', 'UBL-CataloguePricingUpdate-2.4.xsd', 'UBL-CatalogueRequest-2.4.xsd'
@@ -15,5 +15,5 @@ const mainXSD=['UBL-Invoice-2.4.xsd','UBL-ApplicationResponse-2.4.xsd', 'UBL-Att
              , 'UBL-PurchaseReceipt-2.4.xsd', 'UBL-QualificationApplicationRequest-2.4.xsd'
              , 'UBL-QualificationApplicationResponse-2.4.xsd', 'UBL-Quotation-2.4.xsd', 'UBL-ReceiptAdvice-2.4.xsd'
              , 'UBL-Reminder-2.4.xsd', 'UBL-RemittanceAdvice-2.4.xsd', 'UBL-RequestForQuotation-2.4.xsd'
-             , 'UBL-RetailEvent-2.4.xsd', 'UBL-SelfBilledCreditNote-2.4.xsd', 'UBL-SelfBilledInvoice-2.4.xsd'
+             , 'UBL-RetailEvent-2.4.xsd', 'UBL-SelfBilledCreditNote-2.4.xsd', 'UBL-SelfBilledInvoice-2.4.xsd'];
 const partialURL = "https://cdn.jsdelivr.net/gh/rogerburton/smartApp@main/xsd/" ;
