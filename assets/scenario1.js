7d1ac588ae246ee7c42decafb94f027651e7de04
Roger Burton <bur@smartbe.be>
2026-04-22 21:47:30 +0200

    Update scenario1.js

--- assets/scenario1.js
+++ assets/scenario1.js
@@ -1,28 +1,107 @@
 var scenario1={
-  var g1 : `flowchart LR
+g1 : `flowchart LR
 subgraph sg_etape1[REA - une présentation]
-  subgraph rea_doc_initial['Un exemple']
-          SD_i1000[agent A]
-          SD_i1001[agent B]
-          SD_i1002[general goods and services]
-          SD_i1003[money]
+  subgraph rea_doc_initial2[Ex. : achat par une UP]
+          SD_i2000[FOU FFF001]
+          SD_i2001[UP 489000001]
+          SD_i2002["\`general
+goods and services\`"]
+          SD_i2003[Money]
+          SD_i2000-- owns -->SD_i2002
+          SD_i2000-- wants -->SD_i2003
+          SD_i2001-- owns -->SD_i2003
+          SD_i2001-- wants -->SD_i2002
+  end
+  subgraph rea_doc_initial1[Ex. : vente par une UP]
+          SD_i1000[UP 489000001]
+          SD_i1001[CLI CCC001]
+          SD_i1002["\`general
+goods and services\`"]
+          SD_i1003[Money]
           SD_i1000-- owns -->SD_i1002
           SD_i1000-- wants -->SD_i1003
           SD_i1001-- owns -->SD_i1003
           SD_i1001-- wants -->SD_i1002
-  end  subgraph sg_texte1[Le modèle REA]
+  end
+  subgraph sg_texte1[Le modèle REA]
     texte1["\`
   Le modèle REA est basé sur un triplet Ressources/Agents/Evènements.
   Un agent A possède (owns ou plus neutre :  has custody) des ressources X.
   Un autre agent B possède des ressources Y.
   Chaque agent convoite (covets, desires, wants) les ressources de l'autre.
   La négociation consiste à s'accorder entre agents sur l'équivalence des ressources à échanger, en nature, qualité et quantité.
+  Les deux exemples vente et achat montrent que ces deux opérations se représentent exactement de la même manière.
     \`"]
   end
 end
 `,
+g2 : `flowchart LR
+subgraph recipient[Les destinataires des déclarations]
+  ex200[par ex.: ONSS, client, salarié, Intervat, ...]
+end
+subgraph sg_rules[Rules]
+ ex0[blablabla]
+end
+subgraph sg_projections[Projections]
+    subgraph sg_projection_comptable[Projection comptable]
+      ex1[blablabla]
+    end
+    subgraph sg_projection_declarative[Projection déclarative]
+      ex2[blablabla]
+    end
+end
+subgraph sg_plan_conformite[Régulation de la conformité]
+    subgraph sg_regulation_declarative[Régulation déclarative]
+      ex3[blablabla]
+    end
+    subgraph sg_regulation_business[Régulation déclarative]
+      ex4[blablabla]
+    end
+    subgraph sg_regulation_onto[Ontologies]
+      ex44[blablabla]
+    end
+end
+subgraph sg_plan_rea[Plan Business REA]
+    subgraph sg_rea_additional[rea Additional]
+      subgraph rea_vat[add. a]
+        ex5[blablabla]
+      end
+    end
+    subgraph sg_rea_secondary[rea Secondary]
+      subgraph rea_cmf[sec. a]
+        ex6[blablabla]
+      end
+      subgraph rea_shared_vat[sec. b]
+        ex7[blablabla]
+      end
+    end
+    subgraph sg_saisie_doc[Saisie documentaire]
+        ex8[blablabla]
+    end
+end
+subgraph user[Utilisateur]
+  ex10[par ex.: le titulaire d'une UP]
+end
+subgraph sg_texte2[Les plans containers]
+    texte1["\`
+Notre modèle d'affaires,selon moi en même temps modèles organisationnel, conceptuel et du système applicatif repose sur plusieurs plans.
+\`"]
+end
+user -- entrée des spécifications de l'échange --> sg_saisie_doc
+sg_projections -- déclarations --> recipient
+sg_rules -- spécifie --> sg_projections
+sg_plan_conformite -- spécifie --> sg_rules
+sg_plan_conformite -- spécifie --> sg_projections
+sg_plan_rea -- appelle --> sg_rules
+sg_plan_conformite -- spécifie --> sg_plan_rea
+`,
 
-var g1 : `flowchart LR
+
+
+
+
+  
+gx : `flowchart LR
 subgraph sg_rules[Rules]
     subgraph sg_events_rules[EventsRules]
       RL_e1000[règle delai]
